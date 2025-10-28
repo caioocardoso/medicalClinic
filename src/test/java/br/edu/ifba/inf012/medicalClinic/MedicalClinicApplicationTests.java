b9a6092d14f662c2387a0cf2dce5f24dd88c8300
Caio Oliveira Cardoso <136840071+caioocardoso@users.noreply.github.com>
2025-10-28 19:17:13 -0300

    Add files via upload

---
+++ src/test/java/br/edu/ifba/inf012/medicalClinic/MedicalClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifba.inf012.medicalClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
